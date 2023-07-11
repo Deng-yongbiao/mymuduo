d230ef2d7c214f9708cd7b4bf07880a573050c3a
biaos <dengyb1718@163.com>
2023-07-11 20:29:34 +0800

    Poller基类的实现

---
+++ Poller.h
@@ -0,0 +1,34 @@
+#pragma once
+#include "noncopyable.h"
+#include "Timestamp.h"
+
+#include <vector>
+#include <unordered_map>
+
+class Channel;
+class EventLoop;
+
+// muduo库中多路事件分发器的核心IO复用模块
+class Poller : noncopyable
+{
+
+public:
+    using ChannelList = std::vector<Channel *>;
+    Poller(EventLoop *loop);
+    virtual ~Poller() = default;
+    // 给所有IO复用模型保留统一的接口
+    virtual Timestamp poll(int timeoutMs, ChannelList *activeChannels) = 0;
+    virtual void updateChannel(Channel *channel) = 0;
+    virtual void removeChannel(Channel *channel) = 0;
+    //判断channel是否存在在list中
+    bool hasChannel(Channel *channel) const;
+    // Eventloop 可以通过该接口获取默认的IO复用的具体实现
+    static Poller* newDefaultPoller(EventLoop* loop);
+protected:
+    //map的key是socket的文件描述符sockfd     value是通道Channel
+    using ChannelMap = std::unordered_map<int, Channel*>;
+    ChannelMap channels_;
+
+private:
+    EventLoop *ownerLoop_; // 所属的事件循环EventLoop
+};
