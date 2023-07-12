2bdef95acac99735bab9399ea62884f641973ff7
biaos <dengyb1718@163.com>
2023-07-12 19:11:54 +0800

    EpollPoller类别TODO

--- EPollPoller.cpp
+++ EPollPoller.cpp
@@ -1,14 +1,26 @@
 #include "EPollPoller.h"
+#include "Logger.h"
+
+#include <errno.h>
+#include <unistd.h>
+
+const int kNew = -1;
+const int KAdded = 1;
+const int KDeleted = 2;
 
 EPollPoller::EPollPoller(EventLoop *loop) 
   : Poller(loop),
-    epollfd_(::epoll_create1)
+    epollfd_(::epoll_create1(EPOLL_CLOEXEC)),
+    events_(KInitEventListSize)  //创建epoll_event数组
 {
-
+    if(epollfd_ < 0) {
+        LOG_FATAL("epoll_create error:%d \n", errno);
+    }
 }
 
 EPollPoller::~EPollPoller()
 {
+    ::close(epollfd_);
 }
 
 Timestamp EPollPoller::poll(int timeoutMs, ChannelList *activeChannels)
